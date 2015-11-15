362630ce38fb126ad188a7d7244ec5de20d37bb1
Adam Niederer <Adam.Niederer@gmail.com>
2015-11-15 18:32:06 +0000

    Allow for dual-socket client architecture

--- RequestHandler.java
+++ RequestHandler.java
@@ -7,58 +7,80 @@ import org.json.simple.parser.*;
 public class RequestHandler implements Runnable {
 	
     static ConcurrentHashMap<String, String> d = new ConcurrentHashMap<String, String>(16, 0.9f, 2);
+    static ConcurrentHashMap<String, Socket> sl = new ConcurrentHashMap<String, Socket>(16, 0.9f, 2);
     static ArrayBlockingQueue<RequestHandler> all = new ArrayBlockingQueue<RequestHandler>(32);
     final JSONParser p;
     final Socket s;
     
     public RequestHandler(Socket s) throws InterruptedException {
-		try { all.put(this); } catch (Exception e) {}
-		this.s = s;
-		p = new JSONParser();
+	try { all.put(this); } catch (Exception e) {}
+	this.s = s;
+	p = new JSONParser();
     }
 	
     public void run() {
-		try {
-			String inJSON = RequestHandler.readString(new InputStreamReader(s.getInputStream(), "UTF8"), 64);
-			System.out.println(inJSON);
-			
-			JSONObject j = (JSONObject) p.parse(inJSON);
-			String party = (String) j.get("party");
-			String storedJSON = d.put(party, j.toString());
-			if(storedJSON == null) {
-				while(d.get(party).equals(j.toString()))
-					synchronized(d) { d.wait(); }
-				storedJSON = d.get(party);
-				d.remove(party);
-			} else {
-				synchronized (d) { d.notifyAll(); }
-			}
-			s.getOutputStream().write(storedJSON.getBytes("UTF8"));
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				s.close();
-			} catch (Exception e) {}
-		}      
-    }
-	
-    public void closeSocket() {
-		try {
-			System.out.println("Closing socket...");
-			s.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		} 
-    }
+	try {
+	    String inJSON = RequestHandler.readString(new InputStreamReader(s.getInputStream(), "UTF8"), 64);
 
-	public static String readString(Reader ir, int buffersize) throws IOException {
-		StringBuilder inJSONBuilder = new StringBuilder();
-		char[] buffer = new char[buffersize];
-		do {
-			int i = ir.read(buffer, 0, buffersize);
-			inJSONBuilder.append(buffer, 0, i);
-		} while (ir.ready());
-		return inJSONBuilder.toString();
-	}
+	    if(inJSON.length() == 36) { // New receiving clients send their UUIDs, so we add them to a register and use them to transport data back
+		System.out.println("UUID Detected: " + inJSON);
+		sl.put(inJSON, s);
+		return;
+	    }
+
+	    System.out.println("JSON Detected: " + inJSON);
+	    
+	    JSONObject j = (JSONObject) p.parse(inJSON);
+	    String party = (String) j.get("party");
+	    String uuid = j.get("uuid").toString();
+	    
+	    String storedJSON = d.put(party, j.toString());
+	    if(storedJSON == null) {
+		while(d.get(party).equals(j.toString()))
+		    synchronized(d) { d.wait(); }
+		storedJSON = d.get(party);
+		d.remove(party);
+	    } else {
+		synchronized (d) { d.notifyAll(); }
+	    }
+
+	    while(sl.get(uuid) == null) { // Ensure we have a receiving socket
+		synchronized(sl) { sl.wait(); }
+	    }
+	    
+	    Socket rs = sl.get(uuid);
+	    rs.getOutputStream().write(storedJSON.getBytes("UTF8"));
+	    rs.close();
+	    System.out.println("Closed auxillary socket" + rs.getPort());
+	} catch (Exception e) {
+	    e.printStackTrace();
+	} finally {
+	    try {
+		System.out.println("Closing socket " + s.getPort());
+		s.close();
+	    } catch (Exception e) {
+		System.out.println("Error closing a socket!");
+	    }
+	}      
+    }
+    
+    public void closeSocket() {
+	try {
+	    System.out.println("Closing sockets...");
+	    s.close();
+	} catch (Exception e) {
+	    e.printStackTrace();
+	} 
+    }
+    
+    public static String readString(Reader ir, int buffersize) throws IOException {
+	StringBuilder inJSONBuilder = new StringBuilder();
+	char[] buffer = new char[buffersize];
+	do {
+	    int i = ir.read(buffer, 0, buffersize);
+	    if(i != -1)
+		inJSONBuilder.append(buffer, 0, i);
+	} while (ir.ready());
+	return inJSONBuilder.toString();
+    }
 }
--- Server.java
+++ Server.java
@@ -4,41 +4,41 @@ import java.io.*;
 import java.util.ArrayList;
 
 public class Server {
-	
+    
     final ServerSocket s;
     final ExecutorService e;
     static final ArrayList<Server> all = new ArrayList<Server>();
     
     public Server(int port) throws IOException {
-		s = new ServerSocket(port);
-		e = Executors.newFixedThreadPool(16);
+	s = new ServerSocket(port);
+	e = Executors.newFixedThreadPool(16);
     }
-	
+    
     public void run() {
-		while(true) {
-			try { e.execute(new RequestHandler(s.accept())); } catch (Exception e) {}
-		}
+	while(true) {
+	    try { e.execute(new RequestHandler(s.accept())); } catch (Exception e) {}
+	}
     }
-	
+    
     public void closeSocket() {
-		try { s.close(); } catch (Exception e) {}
+	try { s.close(); } catch (Exception e) {}
     }
     
     public static void main(String[] args) throws InterruptedException, IOException {
-		try {
-			Runtime.getRuntime().addShutdownHook(new Thread() {
-					@Override
-					public void run() {
-						synchronized(Server.all) {
-							for(Server server : all)
-								try { server.closeSocket(); } catch (Exception e) {}
-						}
-						for(int i = 0; i < RequestHandler.all.size(); i++)
-							try { RequestHandler.all.poll(5, TimeUnit.SECONDS).closeSocket(); } catch (Exception e) {}
-					}
-				});
-			Server s = new Server(Integer.parseInt(args[0]));
-			s.run();
-		} catch (Exception e) {}
+	try {
+	    Runtime.getRuntime().addShutdownHook(new Thread() {
+		    @Override
+		    public void run() {
+			synchronized(Server.all) {
+			    for(Server server : all)
+				try { server.closeSocket(); } catch (Exception e) {}
+			}
+			for(int i = 0; i < RequestHandler.all.size(); i++)
+			    try { RequestHandler.all.poll(5, TimeUnit.SECONDS).closeSocket(); } catch (Exception e) {}
+		    }
+		});
+	    Server s = new Server(Integer.parseInt(args[0]));
+	    s.run();
+	} catch (Exception e) {}
     }
 }
